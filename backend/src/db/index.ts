b4ac34e81b190d9eed00b927aa31e0ebc7e8e1d5
5t4mp5 <5t4mp5@gmail.com>
2020-09-16 14:27:26 -0400

    DB schema and tables defined. seed moved to separate file

--- backend/src/db/index.ts
+++ backend/src/db/index.ts
@@ -2,40 +2,15 @@ import * as pg from "pg";
 
 const client = new pg.Client("postgress://localhost/tournament_tracker");
 
-client
-  .connect()
-  .then(() => console.log("connected"))
-  .catch((err) => console.log(err));
+async function dbConnect(_client: pg.Client) {
+  await _client
+    .connect()
+    .then(() => console.log("db connected"))
+    .catch((err) => console.log(err));
 
-client.query(
-  `CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-  DO
-  $do$
-  BEGIN
-    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'format') THEN
-      CREATE TYPE format as ENUM ('round-robin', 'single-elimination');
-    END IF;
-    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'status') THEN
-      CREATE TYPE status as ENUM ('open', 'locked');
-    END IF;
-  CREATE TABLE IF NOT EXISTS tournaments 
-  (id uuid PRIMARY KEY DEFAULT uuid_generate_v4(), 
-  title VARCHAR (255) NOT NULL, 
-  format format NOT NULL, 
-  avatar VARCHAR (255), 
-  status status NOT NULL);
-  CREATE TABLE IF NOT EXISTS competitors(
-    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-    name VARCHAR (255),
-    avatar VARCHAR (255)
-  );
-  CREATE TABLE IF NOT EXISTS users (
-    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-    userame VARCHAR (255),
-    avatar VARCHAR (255),
-    email VARCHAR (255),
-    password VARCHAR (255),
-  );
-  END;
-  $do$;`
-);
+  return _client;
+}
+
+const dbConn = dbConnect(client);
+
+export { dbConn };
---
+++ backend/src/db/seed.ts
@@ -0,0 +1,60 @@
+import { dbConn } from "./index";
+
+dbConn.then((conn) => {
+  conn
+    .query(
+      `CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+  DO
+  $do$
+  BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'format') THEN
+      CREATE TYPE format as ENUM ('round-robin', 'single-elimination');
+    END IF;
+    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'status') THEN
+      CREATE TYPE status as ENUM ('open', 'locked');
+    END IF;
+    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'block') THEN
+      CREATE TYPE block as ENUM ('a', 'b');
+    END IF;
+  CREATE TABLE IF NOT EXISTS tournaments 
+  (id uuid PRIMARY KEY DEFAULT uuid_generate_v4(), 
+  title VARCHAR (255) NOT NULL, 
+  format format NOT NULL, 
+  avatar VARCHAR (255), 
+  status status NOT NULL);
+  CREATE TABLE IF NOT EXISTS competitors(
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    name VARCHAR (255),
+    avatar VARCHAR (255)
+  );
+  CREATE TABLE IF NOT EXISTS users (
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    userame VARCHAR (255),
+    avatar VARCHAR (255),
+    email VARCHAR (255),
+    password VARCHAR (255)
+  );
+  CREATE TABLE IF NOT EXISTS pickers (
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    user_id uuid REFERENCES users (id),
+    tournament_id uuid REFERENCES tournaments (id) 
+  );
+  CREATE TABLE IF NOT EXISTS entrants (
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    competitor_id uuid REFERENCES competitors (id),
+    tournament_id uuid REFERENCES tournaments (id),
+    block block NOT NULL
+  );
+  CREATE TABLE IF NOT EXISTS matches (
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    tournament_id uuid REFERENCES tournaments (id),
+    competitor1_id uuid REFERENCES competitors (id),
+    competitor2_id uuid REFERENCES competitors (id),
+    result VARCHAR (255)
+  );
+  END;
+  $do$;`
+    )
+    .then(() => console.log("DB SEEDED!!!"))
+    .catch((e) => console.error(e));
+});
